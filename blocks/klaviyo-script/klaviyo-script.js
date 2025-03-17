78c25d1c52d893d1cd39f92a9e05683bd8980441
dristy.belel <dristy.belel@teknopoint.in>
2025-03-17 10:20:11 +0530

    build fail fixes

--- blocks/klaviyo-script/klaviyo-script.js
+++ blocks/klaviyo-script/klaviyo-script.js
@@ -2,22 +2,23 @@ import { fetchPlaceholders } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
   const placeholders = await fetchPlaceholders();
-  const [klaviyoClass , klaviyoContent] = block.children;
+  const [klaviyoClass] = block.children;
   const className = klaviyoClass.textContent.trim() || placeholders.klaviyoClassName.trim();
   klaviyoClass.remove();
-  className && block.classList.add(className);
+  if (className) {
+    block.classList.add(className);
+  }
   // block.textContent = '';
 }
 
-document.addEventListener('click',(event)=>{
+document.addEventListener('click', (event) => {
   if (window.innerWidth <= 768) { // will only get in mobile view
-    if(event.target.closest('.klaviyo-form-VTMbFv')){
-      // document.querySelector('.kl-private-reset-css-Xuajs1').classList.remove('dsp-none');
-      if(document.querySelector('.kl-private-reset-css-Xuajs1').classList.contains('dsp-block')){
+    if (event.target.closest('.klaviyo-form-VTMbFv')) {
+      if (document.querySelector('.kl-private-reset-css-Xuajs1').classList.contains('dsp-block')) {
         document.querySelector('.klaviyo-script').classList.add('closed');
         document.querySelector('.klaviyo-script').classList.remove('open');
         document.querySelector('.kl-private-reset-css-Xuajs1').classList.remove('dsp-block');
-      } else{
+      } else {
         document.querySelector('.klaviyo-script').classList.remove('closed');
         document.querySelector('.klaviyo-script').classList.add('open');
         document.querySelector('.kl-private-reset-css-Xuajs1').classList.add('dsp-block');
@@ -25,9 +26,3 @@ document.addEventListener('click',(event)=>{
     }
   }
 });
-
-// if (window.innerWidth <= 768) {
-//   document.querySelector('.kl-private-reset-css-Xuajs1').classList.add('dsp-none');
-//   document.querySelector('.klaviyo-script').classList.add('closed');
-// }
-
