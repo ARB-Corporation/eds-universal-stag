94d96c489ebdb9495a6b6cb7c98f243b7c9145f0
dristy.belel <dristy.belel@teknopoint.in>
2025-03-17 10:06:07 +0530

    kalviyo script integration done

--- blocks/klaviyo-script/klaviyo-script.js
+++ blocks/klaviyo-script/klaviyo-script.js
@@ -8,3 +8,26 @@ export default async function decorate(block) {
   className && block.classList.add(className);
   // block.textContent = '';
 }
+
+document.addEventListener('click',(event)=>{
+  if (window.innerWidth <= 768) { // will only get in mobile view
+    if(event.target.closest('.klaviyo-form-VTMbFv')){
+      // document.querySelector('.kl-private-reset-css-Xuajs1').classList.remove('dsp-none');
+      if(document.querySelector('.kl-private-reset-css-Xuajs1').classList.contains('dsp-block')){
+        document.querySelector('.klaviyo-script').classList.add('closed');
+        document.querySelector('.klaviyo-script').classList.remove('open');
+        document.querySelector('.kl-private-reset-css-Xuajs1').classList.remove('dsp-block');
+      } else{
+        document.querySelector('.klaviyo-script').classList.remove('closed');
+        document.querySelector('.klaviyo-script').classList.add('open');
+        document.querySelector('.kl-private-reset-css-Xuajs1').classList.add('dsp-block');
+      }
+    }
+  }
+});
+
+// if (window.innerWidth <= 768) {
+//   document.querySelector('.kl-private-reset-css-Xuajs1').classList.add('dsp-none');
+//   document.querySelector('.klaviyo-script').classList.add('closed');
+// }
+
