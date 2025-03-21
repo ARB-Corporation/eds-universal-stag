175164e1516313d28887ff52fafb6ea96af8b972
dristy.belel <dristy.belel@teknopoint.in>
2025-03-21 09:52:33 +0530

    removed unsed event and commented code

--- blocks/klaviyo-script/klaviyo-script.js
+++ blocks/klaviyo-script/klaviyo-script.js
@@ -9,35 +9,19 @@ export default async function decorate(block) {
     block.classList.add(className);
   }
   // block.textContent = '';
-  block.addEventListener('click', (event) => {
+  block.addEventListener('click', () => {
     if (window.innerWidth <= 768) { // will only get in mobile view
       // if (this.classList.contains('.klaviyo-form-VTMbFv')) {
-        if (block.querySelector('.kl-private-reset-css-Xuajs1').classList.contains('dsp-block')) {
-          block.classList.add('closed');
-          block.classList.remove('open');
-          block.querySelector('.kl-private-reset-css-Xuajs1').classList.remove('dsp-block');
-        } else {
-          block.classList.remove('closed');
-          block.classList.add('open');
-          block.querySelector('.kl-private-reset-css-Xuajs1').classList.add('dsp-block');
-        }
+      if (block.querySelector('.kl-private-reset-css-Xuajs1').classList.contains('dsp-block')) {
+        block.classList.add('closed');
+        block.classList.remove('open');
+        block.querySelector('.kl-private-reset-css-Xuajs1').classList.remove('dsp-block');
+      } else {
+        block.classList.remove('closed');
+        block.classList.add('open');
+        block.querySelector('.kl-private-reset-css-Xuajs1').classList.add('dsp-block');
+      }
       // }
     }
   });
-  // document.addEventListener('click', (event) => {
-  //   if (window.innerWidth <= 768) { // will only get in mobile view
-  //     if (event.target.closest('.klaviyo-form-VTMbFv')) {
-  //       if (document.querySelector('.kl-private-reset-css-Xuajs1').classList.contains('dsp-block')) {
-  //         document.querySelector('.klaviyo-script').classList.add('closed');
-  //         document.querySelector('.klaviyo-script').classList.remove('open');
-  //         document.querySelector('.kl-private-reset-css-Xuajs1').classList.remove('dsp-block');
-  //       } else {
-  //         document.querySelector('.klaviyo-script').classList.remove('closed');
-  //         document.querySelector('.klaviyo-script').classList.add('open');
-  //         document.querySelector('.kl-private-reset-css-Xuajs1').classList.add('dsp-block');
-  //       }
-  //     }
-  //   }
-  // });
 }
-
